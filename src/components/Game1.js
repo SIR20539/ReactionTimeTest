858d6ae227d9afdf71a6a936505771fb9ffd7457
SIR20539 <siretskiy@gmail.com>
2020-06-13 12:21:27 +0200

    add stop btn to the game1 interface, add best result infromatio

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -11,14 +11,21 @@ const StyledDiv = styled.div`
   background-color: pink;
 
   p,
-  button {
+  div.btnBox {
     position: absolute;
-    top: 30%;
     left: 50%;
     transform: translateX(-50%);
   }
-  button {
+  p {
+    top: 30%;
+  }
+  div.btnBox {
     top: 50%;
+    display: flex;
+    flex-direction: column;
+  }
+  button {
+    margin-bottom: 10px;
   }
 `;
 
@@ -26,10 +33,14 @@ export class Game1 extends React.Component {
   state = {
     gameActive: false,
     time: 0,
+    gameStoped: false,
   };
+  results = [];
+  bestResult = 0;
   timerID = undefined;
   startGame = () => {
     this.setState({
+      time: 0,
       gameActive: true,
     });
     setTimeout(() => {
@@ -39,24 +50,56 @@ export class Game1 extends React.Component {
         });
       }, 10);
     }, Math.floor(Math.random() * 3000) + 1000);
+
+    if (this.state.time !== 0) {
+      this.results.push(this.state.time);
+    } else {
+      this.results = [];
+    }
   };
 
   stopTimer = () => {
     clearInterval(this.timerID);
+    this.setState({
+      gameActive: false,
+    });
+  };
+
+  stopGame = () => {
+    this.results.push(this.state.time);
+    this.bestResult = this.results.sort((a, b) => a - b)[0];
+    this.setState({
+      gameStoped: true,
+    });
+    console.log(this.results);
   };
 
   render() {
-    const { gameActive, time } = this.state;
+    const { gameActive, time, gameStoped } = this.state;
     return (
-      <StyledDiv onClick={gameActive ? this.stopTimer : null}>
-        {!gameActive ? (
+      <StyledDiv
+        style={this.state.time !== 0 ? { backgroundColor: 'red' } : null}
+        onClick={gameActive && time !== 0 ? this.stopTimer : null}
+      >
+        {!gameActive && time === 0 ? (
           <>
-            <p>best result</p>
-            <Button onClick={this.startGame} text="play" />
+            <p>information about game</p>
+            <div className="btnBox">
+              <Button onClick={this.startGame} text="play" />
+            </div>
           </>
-        ) : (
+        ) : gameStoped ? null : (
           <p>{time} ms</p>
         )}
+        {!gameActive && time !== 0 ? (
+          <div className="btnBox">
+            {gameStoped ? null : (
+              <Button text="restart" onClick={this.startGame} />
+            )}
+            <Button text="stop" onClick={this.stopGame} />
+          </div>
+        ) : null}
+        {gameStoped ? <p>best score:{this.bestResult}</p> : null}
       </StyledDiv>
     );
   }
